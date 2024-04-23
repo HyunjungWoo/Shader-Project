c6464b94737bbb1b9e2e1a74b25f50f368892b24
HyunjungWoo <112833211+HyunjungWoo@users.noreply.github.com>
2024-04-23 16:29:40 +0900

    동심원 추가 코드
    + m_FSSandboxTime int-> float로 변경

--- SimpleGame/Shaders/FSSandbox.fs
+++ SimpleGame/Shaders/FSSandbox.fs
@@ -2,8 +2,12 @@
 
 layout(location=0) out vec4 FragColor;
 
+uniform float u_Time;
 in vec4 v_Color;
-void main()
+
+const float c_PI = 3.14;
+
+void FilledCircle()
 {
 	vec4 newColor = vec4(1,1,1,1);
 	float r = 0.5;
@@ -18,4 +22,38 @@ void main()
 		newColor = vec4(0,0,0,0);
 	}
 	FragColor = newColor;
+}
+
+void Circle()
+{
+	vec4 newColor = vec4(1,1,1,1);
+	float r		= 0.5;
+	float width = 0.02; // 굵기 조절 
+	vec2 center = vec2(0.5,0.5);
+	float dist  = distance(v_Color.rg, center);
+	if( dist > r - width && dist < r )
+	{
+		newColor = vec4(1,1,1,1);
+	}
+	else
+	{
+		newColor = vec4(0,0,0,0);
+	}
+	
+	FragColor = newColor;
+}
+
+void Circles()
+{
+	float circleCount	= 10; // 동심원의 개수 0~1
+	vec2  ciecleCenter	= vec2(0.5,0.5);
+	float dist			= distance(v_Color.rg,ciecleCenter);
+	float Input			= circleCount * c_PI  * 2  *dist + u_Time; 
+	float sinValue		= pow(sin(Input), 16);
+	FragColor			= vec4(sinValue);
+}
+void main()
+{
+	//FilledCircle();
+	Circles();
 }
